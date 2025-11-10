d0488c9bb97ba74606b00529e21d81f222e7f156
Arroch35 <arrocha.0305@gmail.com>
2025-11-10 18:28:43 +0100

    jana config

--- src/config.py
+++ src/config.py
@@ -1,14 +1,8 @@
 from pathlib import Path
 
-YOUR_PATH_TO_THE_PROJECT = r"C:\Users\arroc\OneDrive\Escritorio\Apuntes\UAB\4th_year\VAL\github_projects"
+DATA_DIR = Path("C:\Users\janaz\Documents\uni\YEAR 4 - S1\vision and learning\CrossValidation")
 
-ROOT_DIR = Path(YOUR_PATH_TO_THE_PROJECT) / "Medical_imaging"
-DATA_DIR = ROOT_DIR / "data"
-MODELS_DIR = ROOT_DIR / "models"
-SRC_DIR = ROOT_DIR / "src"
-
-CROSS_VALIDATION_DIR = DATA_DIR / "CrossValidation"
-CROPPED_PATCHES_DIR = CROSS_VALIDATION_DIR / "Cropped"
-ANNOTATED_PATCHES_DIR = CROSS_VALIDATION_DIR / "Annotated"
-ANNOTATED_METADATA_FILE = CROSS_VALIDATION_DIR / "HP_WSI-CoordAllAnnotatedPatches.xlsx"
-PATIENT_DIAGNOSIS_FILE = CROSS_VALIDATION_DIR / "PatientDiagnosis.csv"
+CROPPED_PATCHES_DIR = DATA_DIR / "Cropped"
+ANNOTATED_PATCHES_DIR = DATA_DIR / "Annotated"
+ANNOTATED_METADATA_FILE = DATA_DIR / "HP_WSI-CoordAllAnnotatedPatches.xlsx"
+PATIENT_DIAGNOSIS_FILE = DATA_DIR / "PatientDiagnosis.csv"
